e63a682a28cf592e26dd448dc925e6468d207a60
Pravallika-Manthenna <51950359+Pravallika-Manthenna@users.noreply.github.com>
2019-07-29 15:09:30 +0530

    Update TrackServiceImpl.java

--- src/main/java/com/stackroute/MuzixAppMysql/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/MuzixAppMysql/service/TrackServiceImpl.java
@@ -1,107 +1,4 @@
 package com.stackroute.MuzixAppMysql.service;
-//
-//import com.stackroute.MuzixAppMysql.repository.UserRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//public class TrackServiceimpl {
-//}
-//package com.stackroute.MuzixAppMysql.service;
-//        import com.stackroute.MuzixAppMysql.domain.User;
-//        import com.stackroute.MuzixAppMysql.repository.UserRepository;
-//
-//        import org.springframework.beans.factory.annotation.Autowired;
-//        import org.springframework.stereotype.Service;
-//        import java.util.List;
-//        import java.util.Optional;
-//
-//
-//@Service
-//public class UserServiceImpl implements UserService {
-//
-//    UserRepository userRepository;
-//
-//    @Autowired
-//    public UserServiceImpl(UserRepository userRepository) {
-//        this.userRepository = userRepository;
-//
-//    }
-//
-//    @Override
-//    public User saveUser(User user) {
-//        User savedUser = userRepository.save(user);
-//
-//        return savedUser;
-//    }
-//
-//    @Override
-//    public List<User> getAllUsers() {
-//
-//        return userRepository.findAll();
-//    }
-//
-//
-//    public User updateUser(User user, int id) throws Exception {
-//        Optional<User> userOptional = userRepository.findById(id);
-//
-//        if (!userOptional.isPresent())
-//            throw new Exception("user id not found");
-//
-//
-//        user.setId(id);
-//
-//        return userRepository.save(user);
-//
-//    }
-//
-//    @Override
-//    public boolean deleteuser(int id) throws Exception {
-//        Optional<User> userOptional = userRepository.findById(id);
-//
-//        if (!userOptional.isPresent())
-//            throw new Exception("user id not found");
-//        userRepository.delete(userOptional.get());
-//        return true;
-//    }
-//
-//    @Override
-//    public List<User> searchUser(String searchString) {
-//        return userRepository.searchUser(searchString);
-//    }
-//
-//    @Override
-//    public List<User> getUserByName(String name) {
-//
-//        return userRepository.getUserByName(name);
-//
-//        @Override
-//        public boolean deleteuser ( int id) throws Exception {
-//            Optional<User> userOptional = userRepository.findById(id);
-//
-//            if (!userOptional.isPresent())
-//                throw new Exception("user id not found");
-//            userRepository.delete(userOptional.get());
-//            return true;
-//        }
-//
-//        @Override
-//        public List<User> searchUser (String searchString){
-//            return userRepository.searchUser(searchString);
-//        }
-
-//            @Override
-//    public List<User> searchUser(String name, int id) {
-//        System.out.println(name);
-//        System.out.println(id);
-//        return userRepository.searchUser(name,id);
-//  }
-//
-//
-//    }
-//}
 
 import com.stackroute.MuzixAppMysql.domain.Track;
 import com.stackroute.MuzixAppMysql.exceptions.TrackAlreadyExistsException;
@@ -109,21 +6,23 @@ import com.stackroute.MuzixAppMysql.exceptions.TrackNotFoundException;
 import com.stackroute.MuzixAppMysql.repository.TrackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
+//service annotation is used to make this class as a service class
 @Service
 public class TrackServiceImpl implements TrackService{
 
     TrackRepository trackRepository;
 
+    //autowired annotation is used 
     @Autowired
     public TrackServiceImpl(TrackRepository trackRepository)
     {
         this.trackRepository = trackRepository;
     }
 
+    //implementation of save track method
     @Override
     public Track saveTrack(Track track) throws TrackAlreadyExistsException {
 
@@ -140,12 +39,14 @@ public class TrackServiceImpl implements TrackService{
         return savedTrack;
     }
 
+    //implementation of get all tracks method
     @Override
     public List<Track> getAllTracks() {
 
         return trackRepository.findAll();
     }
 
+    //implementation of get tracks by name method
     @Override
     public List<Track> getTracksByName(String name) {
 
@@ -153,6 +54,7 @@ public class TrackServiceImpl implements TrackService{
 
     }
 
+    //implementation of update track method
     public Track updateTrack(Track track, int id) throws TrackNotFoundException
     {
         Optional<Track> track1 = trackRepository.findById(id);
@@ -168,6 +70,7 @@ public class TrackServiceImpl implements TrackService{
         return savedTrack;
     }
 
+    //implementation of delete track method
     public boolean deleteTrack(int id) throws TrackNotFoundException
     {
         Optional<Track> track1 = trackRepository.findById(id);
@@ -190,6 +93,7 @@ public class TrackServiceImpl implements TrackService{
         }
     }
 
+    //implementation of search tracks method
     @Override
     public List<Track> searchTracks(String searchString) {
 
